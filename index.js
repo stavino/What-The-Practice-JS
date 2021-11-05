e298380701190711a48f430fdfb351dae7156ad3
Stephen Avino <stephen.avino@gmail.com>
2021-11-05 12:39:40 -0400

    here we gooooo

---
+++ index.js
@@ -0,0 +1,36 @@
+//DOM STUFF
+
+let something = document.querySelector("h1")
+console.log("This is Something ->", something);
+
+something.textContent = "wow this is cool!";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//LOGIC STUFF
+
+// console.log("😃");
+// let variable1 = 1;
+
+// function changeAVariable(placeHolder){
+    
+//     console.log(`you changed ${placeHolder} to` + placeHolder.tostring)
+// }
+// changeAVariable(variable1);
